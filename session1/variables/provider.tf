1462d7de0a866cea2ed239a432d1c3fa5729136b
nivasgoud <nivaswl12@gmail.com>
2024-08-09 17:32:10 +0530

    Terraform core

--- session1/variables/provider.tf
+++ session1/variables/provider.tf
@@ -1,5 +1,8 @@
-#cmd line --var="inst_type=t3.small"
-#
+#cmd line  -var="inst_type=t3.small"
+#var-file  -var-file=roboshop.tfvars-->inst_type=t3.large"
+#terraform var file-->inst_type=t3.medium
+#variable file
+#terraform Environment variable--> set or export TF_VAR_inst_type=t2.large
 
 
 terraform {
